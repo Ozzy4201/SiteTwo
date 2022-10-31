3e880879c8389aeff0a8ddf41dce8084bbbd2020
Ozzy4201 <osborn.nyberg@gmail.com>
2022-10-31 15:24:05 +0100

    Added some text

--- js/onclick.js
+++ js/onclick.js
@@ -4,14 +4,6 @@ const Middle = document.getElementById("Middle");
 const Bottom = document.getElementById("Bottom");
 var open = 0.5;
 
-function animateTopMidBot(Top, Middle, Bottom) {
-    if(open=0){
-        console.log("open = 0");
-    };
-    if(open=1){
-        console.log("open = 1");
-    }
-    return 0;
-};
-
-burger.addEventListener("click", animateTopMidBot(Top, Middle, Bottom));
+burger.addEventListener("click", () => {
+    
+});
--- js/parralax.js
+++ js/parralax.js
@@ -1,48 +1,9 @@
 const scrollEl = document.documentElement
 const body = document.body;
 const main = document.getElementById('custom');
-
-let sx = 0, // For scroll positions
-    sy = 0;
-let dx = sx, // For container positions
-    dy = sy;
-
 let scrollPos
 let b = scrollPos
 let s = 0
-
-
-
-
-window.addEventListener('scroll', easeScroll);
-
-function easeScroll() {
-  
-  sx = window.pageXOffset;
-  sy = window.pageYOffset;
-}
-
-window.requestAnimationFrame(render);
-
-function render(){
-  //We calculate our container position by linear interpolation method
-  dx = li(dx,sx,0.07);
-  dy = li(dy,sy,0.07);
-  
-  dx = Math.floor(dx * 100) / 100;
-  dy = Math.floor(dy * 100) / 100;
-  
-  
-  main.style.transform = `translate3d(-${dx}px, -${dy}px, 0px)`;
-  
-  window.requestAnimationFrame(render);
-}
-
-function li(a, b, n) {
-  return (1 - n) * a + n * b;
-}
-
-
 
 function parallax() {
   if (scrollPos !== scrollEl.scrollTop) {
