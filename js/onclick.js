b6e78e6dc3af388874f0f4c999d68f5bccb4044f
Ozzy <94714886+Ozzy4201@users.noreply.github.com>
2023-02-03 22:18:15 +0100

    Delete js directory

--- js/onclick.js
+++
@@ -1,9 +0,0 @@
-const burger = document.getElementsByClassName("burger")[0];
-const Top = document.getElementById("Top");
-const Middle = document.getElementById("Middle");
-const Bottom = document.getElementById("Bottom");
-var open = 0.5;
-
-burger.addEventListener("click", () => {
-    
-});
